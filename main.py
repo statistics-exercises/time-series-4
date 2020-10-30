77766871f9c3503286749d1156fd9f41e92ac638
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 18:22:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def exponential(lam):
 lamd = 2      # The parameter for the exponential random variable you should sample
 xmax = 6     # The maximum value for x that you should use in your histogram
 nbins = 200   # The number of bins you should use for your histogram
-histo = nbins*[0]     # The variable you should use to store the histogram
-xvals = nbins*[0]     # The midpoints of your histogram bins that are used for plotting
+histo = np.zeros(nbins)     # The variable you should use to store the histogram
+xvals = np.zeros(nbins)     # The midpoints of your histogram bins that are used for plotting
 
 # Your code to compute an estimate for the probability density function of an exponential random
 # variable goes here.
